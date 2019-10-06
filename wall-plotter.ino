ed542b4e24bd7e5cb1db1cda3c5639f378d547b8
ivosdc <ibozic@mail.de>
2019-10-06 17:55:45 +0200

    fix getDistance currentPosition

--- wall-plotter.ino
+++ wall-plotter.ino
@@ -32,154 +32,150 @@ float lastY = 0;
 char plotJson[] = "{\"lines\":[{\"points\":[{\"x\":\"20.0\",\"y\":\"30.0\"},{\"x\":\"8.31\",\"y\":\"-22.00\"},{\"x\":\"-3.22\",\"y\":\"0.00\"},{\"x\":\"-6.76\",\"y\":\"17.97\"},{\"x\":\"-6.80\",\"y\":\"-17.97\"},{\"x\":\"-3.22\",\"y\":\"0.00\"},{\"x\":\"8.33\",\"y\":\"22.00\"},{\"x\":\"3.36\",\"y\":\"0.00\"}]},{\"points\":[{\"x\":\"15.86\",\"y\":\"0.00\"},{\"x\":\"3.75\",\"y\":\"0.00\"},{\"x\":\"4.72\",\"y\":\"-19.09\"},{\"x\":\"4.67\",\"y\":\"19.09\"},{\"x\":\"3.75\",\"y\":\"0.00\"},{\"x\":\"5.56\",\"y\":\"-22.00\"},{\"x\":\"-3.09\",\"y\":\"0.00\"},{\"x\":\"-4.86\",\"y\":\"19.19\"},{\"x\":\"-4.69\",\"y\":\"-19.19\"},{\"x\":\"-2.69\",\"y\":\"0.00\"},{\"x\":\"-4.73\",\"y\":\"19.19\"},{\"x\":\"-4.85\",\"y\":\"-19.19\"},{\"x\":\"-3.09\",\"y\":\"0.00\"},{\"x\":\"5.55\",\"y\":\"22.00\"}]},{\"points\":[{\"x\":\"36.20\",\"y\":\"-21.75\"},{\"x\":\"-1.06\",\"y\":\"-0.16\"},{\"x\":\"-2.05\",\"y\":\"-0.09\"},{\"x\":\"-4.26\",\"y\":\"0.55\"},{\"x\":\"-1.35\",\"y\":\"1.11\"},{\"x\":\"-1.35\",\"y\":\"1.09\"},{\"x\":\"-0.68\",\"y\":\"1.51\"},{\"x\":\"0.00\",\"y\":\"1.91\"},{\"x\":\"0.00\",\"y\":\"1.65\"},{\"x\":\"0.48\",\"y\":\"1.29\"},{\"x\":\"0.97\",\"y\":\"0.92\"},{\"x\":\"0.97\",\"y\":\"0.93\"},{\"x\":\"1.57\",\"y\":\"0.68\"},{\"x\":\"2.18\",\"y\":\"0.44\"},{\"x\":\"0.00\",\"y\":\"0.00\"},{\"x\":\"1.75\",\"y\":\"0.34\"},{\"x\":\"0.00\",\"y\":\"0.00\"},{\"x\":\"1.45\",\"y\":\"0.26\"},{\"x\":\"1.04\",\"y\":\"0.39\"},{\"x\":\"0.64\",\"y\":\"0.52\"},{\"x\":\"0.63\",\"y\":\"0.52\"},{\"x\":\"0.32\",\"y\":\"0.71\"},{\"x\":\"0.00\",\"y\":\"0.91\"},{\"x\":\"0.00\",\"y\":\"1.05\"},{\"x\":\"-0.45\",\"y\":\"0.81\"},{\"x\":\"-0.91\",\"y\":\"0.57\"},{\"x\":\"-0.90\",\"y\":\"0.56\"},{\"x\":\"-1.27\",\"y\":\"0.28\"},{\"x\":\"-1.66\",\"y\":\"0.00\"},{\"x\":\"-1.09\",\"y\":\"0.00\"},{\"x\":\"-1.12\",\"y\":\"-0.08\"},{\"x\":\"-1.13\",\"y\":\"-0.17\"},{\"x\":\"-1.14\",\"y\":\"-0.16\"},{\"x\":\"-1.15\",\"y\":\"-0.25\"},{\"x\":\"-1.18\",\"y\":\"-0.34\"},{\"x\":\"0.00\",\"y\":\"0.00\"},{\"x\":\"0.00\",\"y\":\"3.00\"},{\"x\":\"0.00\",\"y\":\"0.00\"},{\"x\":\"1.23\",\"y\":\"0.34\"},{\"x\":\"1.18\",\"y\":\"0.25\"},{\"x\":\"1.14\",\"y\":\"0.16\"},{\"x\":\"1.14\",\"y\":\"0.17\"},{\"x\":\"1.08\",\"y\":\"0.08\"},{\"x\":\"1.03\",\"y\":\"0.00\"},{\"x\":\"2.68\",\"y\":\"0.00\"},{\"x\":\"2.04\",\"y\":\"-0.52\"},{\"x\":\"1.39\",\"y\":\"-1.04\"},{\"x\":\"1.39\",\"y\":\"-1.04\"},{\"x\":\"0.70\",\"y\":\"-1.54\"},{\"x\":\"0.00\",\"y\":\"-2.03\"},{\"x\":\"0.01\",\"y\":\"-1.74\"},{\"x\":\"-0.51\",\"y\":\"-1.38\"},{\"x\":\"-1.02\",\"y\":\"-1.02\"},{\"x\":\"-1.01\",\"y\":\"-1.03\"},{\"x\":\"-1.56\",\"y\":\"-0.72\"},{\"x\":\"-2.13\",\"y\":\"-0.41\"},{\"x\":\"0.00\",\"y\":\"0.00\"},{\"x\":\"-1.73\",\"y\":\"-0.34\"},{\"x\":\"0.00\",\"y\":\"0.00\"},{\"x\":\"-1.56\",\"y\":\"-0.29\"},{\"x\":\"-1.06\",\"y\":\"-0.36\"},{\"x\":\"-0.57\",\"y\":\"-0.43\"},{\"x\":\"-0.56\",\"y\":\"-0.43\"},{\"x\":\"-0.29\",\"y\":\"-0.62\"},{\"x\":\"-0.01\",\"y\":\"-0.81\"},{\"x\":\"0.00\",\"y\":\"-1.04\"},{\"x\":\"0.43\",\"y\":\"-0.80\"},{\"x\":\"0.87\",\"y\":\"-0.57\"},{\"x\":\"0.86\",\"y\":\"-0.56\"},{\"x\":\"1.23\",\"y\":\"-0.28\"},{\"x\":\"1.59\",\"y\":\"0.00\"},{\"x\":\"0.93\",\"y\":\"0.00\"},{\"x\":\"0.95\",\"y\":\"0.09\"},{\"x\":\"0.98\",\"y\":\"0.16\"},{\"x\":\"0.99\",\"y\":\"0.17\"},{\"x\":\"1.05\",\"y\":\"0.25\"},{\"x\":\"1.12\",\"y\":\"0.33\"},{\"x\":\"0.00\",\"y\":\"0.00\"},{\"x\":\"0.00\",\"y\":\"-3.00\"},{\"x\":\"0.00\",\"y\":\"0.00\"},{\"x\":\"-1.11\",\"y\":\"-0.33\"},{\"x\":\"-1.08\",\"y\":\"-0.25\"},{\"x\":\"-1.06\",\"y\":\"-0.17\"}]}]}";
 
 int initServer() {
-  int retries = 0;
-  Serial.println("Connecting...");
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-  while ((WiFi.status() != WL_CONNECTED) && (retries < WIFI_INIT_RETRY)) {
+    int retries = 0;
+    Serial.println("Connecting...");
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+    while ((WiFi.status() != WL_CONNECTED) && (retries < WIFI_INIT_RETRY)) {
       retries++;
       delay(1000);
       Serial.print("#");
-  }
-  
-  server.begin();
-  Serial.println(WiFi.localIP());
-  serverRouting();
+    }
 
-  return WiFi.status();
+    server.begin();
+    Serial.println(WiFi.localIP());
+    serverRouting();
+
+    return WiFi.status();
 }
 
 void initMotors() {
-  motorL.setStepDuration(motorSpeed);
-  motorR.setStepDuration(motorSpeed);
+    motorL.setStepDuration(motorSpeed);
+    motorR.setStepDuration(motorSpeed);
 
-  servoPen.attach(SERVO_PIN);
-  servoPen.write(PEN_UP);
+    servoPen.attach(SERVO_PIN);
+    servoPen.write(PEN_UP);
 }
 
 bool initPlot(String json) {
-  if (DeserializationError error = deserializeJson(doc, json)) {
+    if (DeserializationError error = deserializeJson(doc, json)) {
         Serial.println("error parsing json");
         server.send(400);
 
         return false;
-  }
+    }
+    printing = true;
+    server.sendHeader("Location", "/plot/");
+    server.send(201);
 
-  return true;
+
+    return true;
 }
 
 void postPlot() {
-  String body = server.arg("plain");
-  if (initPlot(body)) {
-      printing = true;
-      server.sendHeader("Location", "/plot/");
-      server.send(201);
-  }
+    String body = server.arg("plain");
+    initPlot(body);
 }
 
 void serverRouting() {
-  server.on("/", HTTP_GET, []() {
-      server.send(200, "text/html",
-          "/plot");
-  });
-  server.on("/plot", HTTP_POST, postPlot);
+    server.on("/", HTTP_GET, []() {
+        server.send(200, "text/html", "/plot");
+    });
+    server.on("/plot", HTTP_POST, postPlot);
 }
 
 bool getPoint(int line, int point, float *x, float* y)
 {
-  float newX = doc["lines"][line]["points"][point]["x"];
-  float newY = doc["lines"][line]["points"][point]["y"];
-  if (newX == 0 && newY == 0) {
-    
-    return false;
-  }
-  *x = newX;
-  *y = newY;
-    
-  return true; 
+    float newX = doc["lines"][line]["points"][point]["x"];
+    float newY = doc["lines"][line]["points"][point]["y"];
+    if (newX == null || newY == null) {
+
+        return false;
+    }
+    *x = newX;
+    *y = newY;
+
+    return true;
 }
 
-void drawLine(long distanceR, long distanceL){
-  Serial.println("NEWLine");
-  int lDirection = 1;
-  int rDirection = -1;
-  if (distanceL < 0) {
-    lDirection = lDirection * -1;
-  }
-  if (distanceR < 0) {
-    rDirection = rDirection * -1;
-  }
-  long distL = distanceL;
-  long distR = distanceR;
-  if (distanceL < 0) {
-    distL = distL * -1;
-  }
-  if (distanceR < 0) {
-    distR = distR * -1;
-  }
-  long ticks = distL * distR;
-  for (long i = 0; i < ticks; i++) {
-    if (i % distR == 0) {
-       motorL.step(STEPS_PER_TICK * lDirection);
+void drawLine(long distanceL, long distanceR){
+    int directionL = 1;
+    int directionR = -1;
+    long distL = distanceL;
+    long distR = distanceR;
+    if (distanceL < 0) {
+        directionL = directionL * -1;
+        distL = distL * -1;
     }
-    if (i % distL == 0) {
-       motorR.step(STEPS_PER_TICK * rDirection);
+    if (distanceR < 0) {
+        directionR = directionR * -1;
+        distR = distR * -1;
     }
-  }
+    long ticks = distL * distR;
+    for (long i = 1; i <= ticks; i++) {
+        if (i % distL == 0) {
+            motorL.step(STEPS_PER_TICK * directionL);
+        }
+        if (i % distR == 0) {
+            motorR.step(STEPS_PER_TICK * directionR);
+        }
+    }
 }
 
 void getDistance(float x, float y, long *distanceLeft, long *distanceRight) {
-  float nextX = x * zoom + lastX;
-  float nextY = y * zoom + lastY;
-  float xPosL = nextX + centerX;
-  float xPosR = nextX + centerX - canvasWidth;
-  float yPos  = nextY + centerY;
-  long newLeft  = sqrt(xPosL * xPosL + yPos * yPos);
-  long newRight = sqrt(xPosR * xPosR + yPos * yPos);
-  *distanceLeft  = (newLeft  - currentLeft);
-  *distanceRight = (newRight - currentRight);
-  currentLeft = newLeft;
-  currentRight = newRight;
-  lastX = nextX;
-  lastY = nextY;
+    float nextX = x * zoom + lastX;
+    float nextY = y * zoom + lastY;
+    float xPos1 = nextX + centerX;
+    float xPos2 = nextX + centerX - canvasWidth;
+    float yPos  = nextY + centerY;
+    long newXPos1  = sqrt(pow(xPos1, 2) + pow(yPos, 2));
+    long newXPos2 = sqrt(pow(xPos2, 2) + pow(yPos, 2));
+    *distanceRight  = (newXPos1  - currentLeft);
+    *distanceLeft = (newXPos2 - currentRight);
+    currentLeft = newXPos2;
+    currentRight = newXPos1;
+    lastX = nextX;
+    lastY = nextY;
 }
 
 void setup()
 {
-  Serial.begin(9600); 
-  Serial.println("Setup");
-  Serial.print("Canvas width:");
-  Serial.println(canvasWidth);
+    Serial.begin(9600);
+    Serial.println("Setup");
+    Serial.print("Canvas width:");
+    Serial.println(canvasWidth);
 
-  initMotors();
-  Serial.println(initServer());
-  Serial.println(initPlot(plotJson));
+    initMotors();
+    Serial.println(initServer());
+    Serial.println(initPlot(plotJson));
 
-  delay(5000);
+    delay(5000);
 }
 
 void loop() {
-  server.handleClient();
-  if (printing) {
-    float tmpX, tmpY = 0;
-    for (int line = 0; line < doc["lines"].size(); line++) {
-      for (int point = 0; point < doc["lines"][line]["points"].size(); point++) {
-        if(!getPoint(line, point, &tmpX, &tmpY)) {
-          servoPen.write(PEN_UP);
-          Serial.println("Plot done");
-          printing = false;
-        } else {
-            if (point == 0) {
-              servoPen.write(PEN_UP);
-            } else {
-              servoPen.write(PEN_DOWN);
+    server.handleClient();
+    if (printing) {
+        for (int line = 0; line < doc["lines"].size(); line++) {
+            for (int point = 0; point < doc["lines"][line]["points"].size(); point++) {
+                float tmpX = 0;
+                float tmpY = 0;
+                if(!getPoint(line, point, &tmpX, &tmpY)) {
+                    servoPen.write(PEN_UP);
+                    Serial.println("Plot done");
+                    printing = false;
+                } else {
+                    if (point == 0) {
+                        servoPen.write(PEN_UP);
+                    } else {
+                        servoPen.write(PEN_DOWN);
+                    }
+                    long distanceLeft = 0;
+                    long distanceRight = 0;
+                    getDistance(tmpX,tmpY, &distanceLeft, &distanceRight);
+                    drawLine(distanceLeft, distanceRight);
+                }
             }
-            long distanceLeft = 0;
-            long distanceRight = 0;
-            getDistance(tmpX,tmpY, &distanceLeft, &distanceRight);
-            drawLine(distanceLeft, distanceRight);
-         }
-      }
+        }
     }
-  }
 }
+
