556bf5d600c92a302ac9646f730ee57a62951e11
ivosdc <ibozic@mail.de>
2019-10-13 19:57:43 +0200

    adds softAccesPoint if wlan is not reachable

--- wall-plotter.ino
+++ wall-plotter.ino
@@ -3,7 +3,8 @@
 #include <StepperMotor.h>
 #include <ESP8266WebServer.h>
 #include "FS.h"
-
+#include <DNSServer.h>
+#include <ESP8266WiFi.h>
 
 #define SERVO_PIN D9
 #define WIFI_INIT_RETRY 10
@@ -31,7 +32,10 @@ Servo servoPen;
 const char* ssid  = "SSID";
 const char* password = "PASSWORD";
 ESP8266WebServer server(80);
+IPAddress accessPointIP(192, 168, 0, 1);  
+IPAddress netMask(255, 255, 255, 0);
 
+DNSServer dnsServer;
 StaticJsonDocument<10000> plotJson;
 StaticJsonDocument<1000> configJson;
 bool printing = true;
@@ -49,6 +53,44 @@ char configData[] = "{\"server\":{\"ssid\":\"ssid\",\"password\":\"password\"},\
 
 char plotData[] = "{\"lines\":[{\"points\":[{\"x\":\"243.98\",\"y\":\"102.51\"},{\"x\":\"6.00\",\"y\":\"-2.00\"},{\"x\":\"5.80\",\"y\":\"-7.39\"},{\"x\":\"-4.52\",\"y\":\"-7.10\"},{\"x\":\"-8.77\",\"y\":\"-3.81\"},{\"x\":\"-26.60\",\"y\":\"-8.78\"},{\"x\":\"-3.10\",\"y\":\"-22.43\"},{\"x\":\"4.09\",\"y\":\"-10.17\"},{\"x\":\"16.57\",\"y\":\"-6.44\"},{\"x\":\"12.55\",\"y\":\"2.22\"},{\"x\":\"2.81\",\"y\":\"0.50\"},{\"x\":\"7.51\",\"y\":\"1.21\"},{\"x\":\"1.66\",\"y\":\"2.24\"},{\"x\":\"1.38\",\"y\":\"1.87\"},{\"x\":\"-0.36\",\"y\":\"5.19\"},{\"x\":\"0.00\",\"y\":\"2.38\"},{\"x\":\"-8.59\",\"y\":\"-3.64\"},{\"x\":\"-10.00\",\"y\":\"-4.57\"},{\"x\":\"-9.41\",\"y\":\"3.47\"},{\"x\":\"-6.16\",\"y\":\"2.27\"},{\"x\":\"-5.21\",\"y\":\"7.11\"},{\"x\":\"4.11\",\"y\":\"6.32\"},{\"x\":\"7.34\",\"y\":\"9.28\"},{\"x\":\"23.81\",\"y\":\"4.64\"},{\"x\":\"6.55\",\"y\":\"7.12\"},{\"x\":\"0.95\",\"y\":\"2.49\"},{\"x\":\"-0.87\",\"y\":\"14.51\"},{\"x\":\"-6.63\",\"y\":\"10.76\"},{\"x\":\"-17.36\",\"y\":\"3.77\"},{\"x\":\"-12.53\",\"y\":\"-0.53\"},{\"x\":\"-3.06\",\"y\":\"-2.62\"},{\"x\":\"-7.11\",\"y\":\"-1.31\"},{\"x\":\"-1.81\",\"y\":\"-2.58\"},{\"x\":\"-1.38\",\"y\":\"-1.96\"},{\"x\":\"0.36\",\"y\":\"-5.08\"},{\"x\":\"0.00\",\"y\":\"-2.45\"},{\"x\":\"10.03\",\"y\":\"4.31\"},{\"x\":\"9.75\",\"y\":\"5.12\"},{\"x\":\"11.22\",\"y\":\"-3.65\"}]},{\"points\":[{\"x\":\"-193.00\",\"y\":\"-64.78\"},{\"x\":\"9.32\",\"y\":\"26.00\"},{\"x\":\"13.68\",\"y\":\"35.00\"},{\"x\":\"15.32\",\"y\":\"-42.00\"},{\"x\":\"1.23\",\"y\":\"-3.32\"},{\"x\":\"4.13\",\"y\":\"-12.19\"},{\"x\":\"2.02\",\"y\":\"-1.89\"},{\"x\":\"2.27\",\"y\":\"-2.14\"},{\"x\":\"5.03\",\"y\":\"0.54\"},{\"x\":\"3.00\",\"y\":\"0.00\"},{\"x\":\"-21.19\",\"y\":\"56.00\"},{\"x\":\"-2.08\",\"y\":\"5.47\"},{\"x\":\"-1.94\",\"y\":\"10.91\"},{\"x\":\"-6.81\",\"y\":\"0.56\"},{\"x\":\"-9.13\",\"y\":\"0.76\"},{\"x\":\"0.56\",\"y\":\"-4.58\"},{\"x\":\"-4.81\",\"y\":\"-12.12\"},{\"x\":\"-21.60\",\"y\":\"-57.00\"},{\"x\":\"11.00\",\"y\":\"0.00\"}]},{\"points\":[{\"x\":\"70.37\",\"y\":\"0.13\"},{\"x\":\"-0.90\",\"y\":\"1.25\"},{\"x\":\"3.40\",\"y\":\"13.62\"},{\"x\":\"2.52\",\"y\":\"10.08\"},{\"x\":\"5.33\",\"y\":\"27.45\"},{\"x\":\"4.28\",\"y\":\"7.47\"},{\"x\":\"14.00\",\"y\":\"-60.00\"},{\"x\":\"12.00\",\"y\":\"0.00\"},{\"x\":\"7.63\",\"y\":\"32.00\"},{\"x\":\"7.37\",\"y\":\"29.00\"},{\"x\":\"10.13\",\"y\":\"-42.00\"},{\"x\":\"0.77\",\"y\":\"-3.09\"},{\"x\":\"2.71\",\"y\":\"-12.56\"},{\"x\":\"1.53\",\"y\":\"-1.78\"},{\"x\":\"1.86\",\"y\":\"-2.17\"},{\"x\":\"5.31\",\"y\":\"0.60\"},{\"x\":\"2.69\",\"y\":\"0.00\"},{\"x\":\"-18.00\",\"y\":\"73.00\"},{\"x\":\"-13.00\",\"y\":\"0.00\"},{\"x\":\"-14.00\",\"y\":\"-61.00\"},{\"x\":\"-2.00\",\"y\":\"0.00\"},{\"x\":\"-14.00\",\"y\":\"61.00\"},{\"x\":\"-13.00\",\"y\":\"0.00\"},{\"x\":\"-18.00\",\"y\":\"-73.00\"},{\"x\":\"4.00\",\"y\":\"0.00\"}]}]}";
 
+void initDNS() {
+    Serial.println("Starting DNS-Server.");
+    dnsServer.setErrorReplyCode(DNSReplyCode::NoError);
+    dnsServer.start(53, "*", accessPointIP);
+}
+
+void initAccessPoint() {  
+    Serial.print("Starting AccessPoint: ");
+    static char szSSID[12];
+    sprintf(szSSID, "WallPlotter %02d", ESP.getChipId() % 100);
+    Serial.println(szSSID);
+    Serial.print("IP: ");
+    Serial.println(accessPointIP);
+    WiFi.mode(WIFI_STA);
+    WiFi.disconnect();
+    delay(100);
+    bool exists = true;
+    while(exists) {
+        int n = WiFi.scanNetworks();
+        exists = false;
+        for (int i=0; i<n; i++) {
+            String ssid = WiFi.SSID(i);
+            if(strcmp(szSSID, ssid.c_str())==0)
+                exists = true;
+        }
+        if(exists) {
+            char accesPointInUse[50];
+            sprintf(accesPointInUse, "AccessPoint '%s' in use, waiting...", szSSID);
+            Serial.println(accesPointInUse);
+            delay(5000);
+        }
+    }
+    WiFi.mode(WIFI_AP);
+    WiFi.softAPConfig(accessPointIP, accessPointIP, netMask);
+    WiFi.softAP(szSSID);
+    delay(100);
+}
+
 void initConfig() {
     configJson["server"]["ssid"] = ssid;
     configJson["server"]["password"] = password;
@@ -60,7 +102,6 @@ void initConfig() {
     configJson["plotter"]["zoomFactor"] = zoomFactor;
     zoom = getZoom(zoomFactor);
     serializeJson(configJson, configData);
-    Serial.println(configData);
 }
 
 bool setConfig() {
@@ -128,14 +169,14 @@ void initFileSystem() {
         f.close();
         f = SPIFFS.open("/config.json", "w");
         if (strlen(configFile) == 0) {
-          Serial.println("writing config.json");
-          Serial.println(configData);
-          f.println(configData);
+            Serial.println("writing config.json");
+            Serial.println(configData);
+            f.println(configData);
         } else {
-          Serial.println("config.json found:");
-          Serial.println(configFile);
-          memcpy(configData,configFile, strlen(configFile));
-          setConfig();
+            Serial.println("config.json found:");
+            Serial.println(configFile);
+            memcpy(configData,configFile, strlen(configFile));
+            setConfig();
         }
     }
     f.close();
@@ -149,6 +190,7 @@ void writeConfig() {
 }
 
 bool initPlot(String json) {
+    Serial.println("Start plotting...");
     if (DeserializationError error = deserializeJson(plotJson, json)) {
         Serial.println("error parsing json");
         server.send(400);
@@ -287,7 +329,10 @@ void setup()
     Serial.println(canvasWidth);
 
     initMotors();
-    Serial.println(initServer());
+    if(initServer() == 1){
+        initDNS();
+        initAccessPoint();
+    }
     Serial.println(initPlot(plotData));
 
     delay(5000);
@@ -325,3 +370,4 @@ void loop() {
         goHome();
     }
 }
+
